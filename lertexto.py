cae3fa6469514fc71f76017869e0bedd4e8eeb02
Butjao <62959137+Butjao@users.noreply.github.com>
2020-06-12 17:11:37 -0300

    Add files via upload

--- lertexto.py
+++ lertexto.py
@@ -1,38 +1,64 @@
-#primeira lida pra contar quantas linhas tem o texto, por qlqr razao depois que le
-#ficou trancado na ultima linha entao tive que fechar e abrir dnv
-f = open('instrucoes.txt','r')
-contador_de_linha=0
-#p cada linha acresce 1 ao contador assim tendo o num de linhas
+#primeira lida pra contar quantas linhas tem o texto, eu nao sei porque dava problema
+#mas fazer isso resolveu
+f = open('entrada01.txt','r')
+contador_de_linha=0 #variavel que conta quantas linhas o arquivo tem
+#pra cada linha acresce 1 ao contador assim tendo o num total de linhas
 for line in f:
     contador_de_linha +=1
-print (contador_de_linha)
+print (contador_de_linha) #MOSTRA QUANTAS LINHAS FORAM LIDAS, PRA GARANTIR
 f.close()  
-#SEGUNDA LIDA
-f = open('instrucoes.txt','r')
-#CRIA 2 VETORES COM O TAMANHO DO TEXTO
+#segunda lida do arquivo
+f = open('entrada01.txt','r')
+#CRIA 2 VETORES COM O TAMANHO DO TEXTO E UM TERCEIRO PRA COMPARAR MUDANCAS
 instrucoes=[0]*contador_de_linha
 enderecos=[0]*contador_de_linha
+compara=[0]*contador_de_linha
 #PREENCHE ELES COM O CONTEUDO DO ARQUIVO
 for n in range(0,contador_de_linha):
-   enderecos[n]=f.read(3)
-   instrucoes[n]=f.read(2)
+   a=f.read(3) #LE 3 CARACTERES (INCLUINDO O ESPACO VAZIO)
+   a=int(a,16) #CONVERTE PRA INTEIRO DE BASE HEXADECIMAL
+   enderecos[n]=hex(a)
+   b=f.read(3)
+   b=int(b,16)
+   instrucoes[n]=hex(b)
+   compara[n]=instrucoes[n] #CRIA UMA COPIA DO VETOR DAS INSTRUCOES
+#PRINTA TODOS OS VALORES SEPARADOS PRA TESTAR SE TA TUDO CERTO
+for n in range(0,contador_de_linha):
+    print(enderecos[n]," ",instrucoes[n]) 
+pc=0 #program counter
+z=0 #   variavel auxiliar
+ac=0 #acum
+ci=0 #cont de instrucoes
+#COMECO DAS INSTRUCOES
+print("Input file:",(f.name),"\n") #MOSTRA O NOME DO ARQUIVO
+while(1>0): #SEMPRE VAI EXECUTAR, SAI DO LAÇO COM A FUCNAO HLT
+    e=instrucoes[pc]
+    if(e==hex(0xf0)): #HLT ENCERRA O PROGRAMA
+        ci+=1
+        pc+=1
+        print("HLT")
+        break
 
+    if(e==hex(0x10)): #LDR CARREGA UM VALOR NO AC
+        ci+=1
+        pc+=1
+        z=instrucoes[pc] #z recebe o endereco da instrucao
+        for n in range(contador_de_linha):
+            if(enderecos[n]==z): #procura um endereço condizente
+                ac = instrucoes[n] 
+                pc+=1
+                print("LDR",enderecos[n])
+                break
 
-#AQUI É SÓ UM TESTE PRA VER SE ELE PEGA O VALOR PS. PRA PEGAR OS ENDERECOS PRECISA CONTAR O 
-#ESPACO DEPOIS ENTAO O ENDERECO "10"  É NA VERDADE "10 "
-if(instrucoes[0]=="10"):
-    print("ook")
-
-#AQUI É O COMECO DO CODIGO DE VERDADE QUE A PRINCIPIO LE TODAS AS LINHAS E TESTA AS INSTRUCOES
-#for n in range(0,contador_de_linha):
-#    if(instrucoes[n]=="")
-
-    
-   
+    if(e==hex(0x20)): #STR GUARDA UM VALOR 
+        ci+=1 
+        pc+=1
+        z=instrucoes[pc] 
+        for n in range(contador_de_linha):
+            if(enderecos[n]==z):   
+                instrucoes[n]=ac  
+                pc+=1  
+                print("STR",enderecos[n])
+                break
 
 f.close()
-    
-
-   
-
-    
